29e1b812b14c2ead8a69ca4f74b62bb0fa7f1e7b
Sven Henderickx <s.henderickx@student.fontys.nl>
2016-09-01 13:08:19 +0200

    test met button

--- HelloWorld/app/src/main/java/com/example/sven/helloworld/HelloWorld.java
+++ HelloWorld/app/src/main/java/com/example/sven/helloworld/HelloWorld.java
@@ -1,7 +1,9 @@
 package com.example.sven.helloworld;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class HelloWorld extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class HelloWorld extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hello_world);
+    }
+
+    public void sendTest(View view){
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }
