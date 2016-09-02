f6b814a8d474f20606fbdd05428260b8668f9b59
Sven Henderickx <s.henderickx@student.fontys.nl>
2016-09-02 09:48:56 +0200

    opdracht 1 af

--- HelloWorld/app/src/main/java/com/example/sven/helloworld/HelloWorld.java
+++ HelloWorld/app/src/main/java/com/example/sven/helloworld/HelloWorld.java
@@ -14,10 +14,6 @@ public class HelloWorld extends AppCompatActivity {
     }
 
     public void sendTest(View view){
-        Intent intent = new Intent(this, DisplayMessageActivity.class);
-        EditText editText = (EditText) findViewById(R.id.edit_message);
-        String message = editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
+
     }
 }
