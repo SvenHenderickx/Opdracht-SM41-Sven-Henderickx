0544ffa6a5a118db28a048f3f44aff62394e88ed
SvenHenderickx <henderickx.sven@gmail.com>
2016-09-28 17:12:55 +0200

    .

--- CSI_Week_1/app/src/main/java/csi/fhict/org/csi_week_1/ReportActivity.java
+++ CSI_Week_1/app/src/main/java/csi/fhict/org/csi_week_1/ReportActivity.java
@@ -4,20 +4,17 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class ReportActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-
+        setContentView(R.layout.activity_report);
     }
 
-    public void goToReport(View view){
-        Intent intent = new Intent(this, ReportActivity.class);
+    public void goBack(View view){
+        Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
 }
